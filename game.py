5efaa989aa65b6590a6ba3f64533900a58867388
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2025-03-13 23:03:04 +0200

    made second screen connection

--- game.py
+++ game.py
@@ -1,18 +1,52 @@
 import sys
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QWidget, QLabel, QPushButton,
-    QVBoxLayout, QDesktopWidget
+    QVBoxLayout, QStackedWidget, QDesktopWidget
 )
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
-class GameScreen(QWidget):
-    def __init__(self):
+class MainMenuScreen(QWidget):
+    def __init__(self, stacked_widget):
         super().__init__()
+        self.stacked_widget = stacked_widget  # Reference to stacked widget to switch screens
         self.init_ui()
 
     def init_ui(self):
-        # Set up the main vertical layout
+        layout = QVBoxLayout(self)
+        layout.setAlignment(Qt.AlignCenter)
+
+        # Set fonts
+        font_large = QFont("Times New Roman", 40)
+        font_medium = QFont("Times New Roman", 20)
+
+        # --- Title Label ---
+        self.title_label = QLabel("Guess Master", self)
+        self.title_label.setFont(font_large)
+        self.title_label.setAlignment(Qt.AlignCenter)
+
+        # --- Start Button ---
+        self.start_button = QPushButton("Start", self)
+        self.start_button.setFont(font_medium)
+        self.start_button.clicked.connect(self.start_game)
+
+        layout.addStretch(1)
+        layout.addWidget(self.title_label)
+        layout.addStretch(1)
+        layout.addWidget(self.start_button)
+        layout.addStretch(2)
+
+    def start_game(self):
+        """Switch to the Game Screen"""
+        self.stacked_widget.setCurrentIndex(1)  # Switch to GameScreen
+
+class GameScreen(QWidget):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        self.stacked_widget = stacked_widget  # Reference to stacked widget to switch screens
+        self.init_ui()
+
+    def init_ui(self):
         layout = QVBoxLayout(self)
         layout.setAlignment(Qt.AlignCenter)
 
@@ -25,10 +59,9 @@ class GameScreen(QWidget):
         self.question_label.setFont(font_large)
         self.question_label.setAlignment(Qt.AlignCenter)
 
-        # Stretch to push the label 25% down
         layout.addStretch(1)
         layout.addWidget(self.question_label)
-        layout.addStretch(1)  # Add space after question before buttons
+        layout.addStretch(1)
 
         # --- Answer Buttons (Stacked Vertically) ---
         self.answer_buttons = {
@@ -39,26 +72,30 @@ class GameScreen(QWidget):
             "I Don't Know": QPushButton("I Don't Know"),
         }
 
-        # Apply font to buttons and stack them vertically
         for btn_text, btn in self.answer_buttons.items():
             btn.setFont(font_medium)
             layout.addWidget(btn)
 
-        layout.addStretch(2)  # Extra stretch below buttons to keep spacing nice
+        layout.addStretch(2)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Guess Master - Game")
+        self.setWindowTitle("Guess Master")
         self.setGeometry(100, 100, 800, 500)
-
-        # Center the window on the screen
         self.center_window()
 
-        # Set the main game screen as the central widget
-        self.game_screen = GameScreen()
-        self.setCentralWidget(self.game_screen)
+        # Create the QStackedWidget
+        self.stacked_widget = QStackedWidget()
+        self.setCentralWidget(self.stacked_widget)
+
+        # Create screens and add them to the stacked widget
+        self.main_menu = MainMenuScreen(self.stacked_widget)
+        self.game_screen = GameScreen(self.stacked_widget)
+
+        self.stacked_widget.addWidget(self.main_menu)  # Index 0
+        self.stacked_widget.addWidget(self.game_screen)  # Index 1
 
     def center_window(self):
         """Centers the main window on the user's screen."""
